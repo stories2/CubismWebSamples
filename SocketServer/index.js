74bca3d558693d619dd7c35deb01d3e2eb4d5f5a
HYUNWOO KIM <stories282@gmail.com>
2020-11-18 23:20:58 +0900

    Simple socket.io server

---
+++ SocketServer/index.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  origins: '*:*', path: '/relay',
+  serveClient: false
+});
+
+app.get('/', (req, res) => {
+  res.send('OK');
+})
+
+io.sockets.on('connection', (socket) => {
+  console.log(`socket [${socket.id}] connected`);
+
+  io.emit('hi', `Hi [${socket.id}]`);
+
+  socket.on('msg', (data) => {
+    console.log(`socket [${socket.id}] msg`, data)
+    socket.broadcast.emit('relay', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`socket [${socket.id}] disconnected`)
+  })
+});
+
+http.listen(5252, () => {
+  console.log('listening on 5252');
+})
